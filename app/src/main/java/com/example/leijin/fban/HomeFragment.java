970b4bdbf2ae0acbb93df95cd07306f4d998df29
jimrex <ijimrex@gmail.com>
2017-04-20 16:36:01 -0700

    About me

--- app/src/main/java/com/example/leijin/fban/HomeFragment.java
+++ app/src/main/java/com/example/leijin/fban/HomeFragment.java
@@ -1,8 +1,10 @@
 package com.example.leijin.fban;
 
 import android.os.Bundle;
+import android.os.Debug;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +17,7 @@ public class HomeFragment extends Fragment{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.home,container,false);
     }
 
