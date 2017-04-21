d7ac3382884b9fe9f3742831f3b04277baedc583
jimrex <ijimrex@gmail.com>
2017-04-20 21:10:58 -0700

    Clear

--- app/src/main/java/com/example/leijin/fban/HomeFragment.java
+++ app/src/main/java/com/example/leijin/fban/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.leijin.fban;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Debug;
 import android.support.annotation.Nullable;
@@ -8,22 +9,52 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by leijin on 4/19/17.
  */
 
-public class HomeFragment extends Fragment{
+
+public class HomeFragment extends Fragment implements View.OnClickListener{
+    EditText content;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.home, container, false);
+        content=(EditText)view.findViewById(R.id.keyword);
 
-        return inflater.inflate(R.layout.home,container,false);
+
+        Button buttonClear = (Button) view.findViewById(R.id.Clear);
+        buttonClear.setOnClickListener(this);
+        Button buttonSearch = (Button) view.findViewById(R.id.Search);
+        buttonSearch.setOnClickListener(this);
+        return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getActivity().setTitle("Search On FB");
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id==R.id.Clear)
+            clearContent();
+        else search();
+
+//        Log.d("in","id");
+
+    }
+    private void clearContent(){
+        content.setText("");
+    }
+    private void search(){
+//        Intent intent = new Intent(getActivity(), ResultFragment.class);
+//        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/leijin/fban/ResultFragment.java
@@ -0,0 +1,8 @@
+package com.example.leijin.fban;
+
+/**
+ * Created by leijin on 4/20/17.
+ */
+
+public class ResultFragment {
+}
